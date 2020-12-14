67018269851708411dc168fd50159110c54c7b19
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-12-15 00:39:47 +0900

    Exit if the container exists

--- scripts/launch-container-linux.sh
+++ scripts/launch-container-linux.sh
@@ -2,6 +2,13 @@
 
 xhost +
 
+if [ "$(docker ps -a | grep lsdslam_noros)" ]
+then
+    echo "Container already exists."
+    echo "Run 'docker start lsdslam_noros; docker attach lsdslam_noros' to start" && \
+    exit
+fi
+
 docker run -it \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
